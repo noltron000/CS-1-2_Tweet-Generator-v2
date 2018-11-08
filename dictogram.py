bf9ea3e4aef1d907e125128742adc4a4e81975cd
Nolan Kovacik <noltron000@gmail.com>
2018-11-08 11:32:32 -0800

    fixing for loop, counting tokens

--- dictogram.py
+++ dictogram.py
@@ -20,10 +20,12 @@ class Dictogram(dict):
 	def add_count(self, word, count=1):
 		"""Increase frequency count of given word by given count amount."""
 		# TODO: Increase word frequency by count
+		self.tokens += 1
 
 	def frequency(self, word):
 		"""Return frequency count of given word, or 0 if word is not found."""
 		# TODO: Retrieve word frequency count
+		self.tokens += 1
 
 def print_histogram(word_list):
 	print('word list: {}'.format(word_list))
@@ -31,7 +33,7 @@ def print_histogram(word_list):
 	histogram = Dictogram(word_list)
 	print(f'dictogram: {histogram}')
 	print(f'{histogram.tokens} tokens, {histogram.types} types')
-	for word in word_list[-2:]:
+	for word in word_list:
 		freq = histogram.frequency(word)
 		print(f"'{word}' occurs {freq} times")
 	print()
