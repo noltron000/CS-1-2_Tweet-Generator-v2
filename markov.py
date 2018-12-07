3936a1daeb75f3c8613d1eba1c7d0bc0de173785
Nolan Kovacik <noltron000@gmail.com>
2018-12-06 21:36:11 -0800

    adding docstrings

--- markov.py
+++ markov.py
@@ -3,22 +3,22 @@ class Markov(object):
 		self.corpus = corpus
 		self.order = order
 
-		self.dict = {}
-		self.phrase = []
 		self.word = ''
+		self.phrase = []
+		self.dict = {}
 
-		self.create()
-		print(self.dict)
-
-	def create(self):
-		for grapheme in self.corpus:
-			if grapheme == ' ': # this can be improved greatly
-				self.saveWord()
-			else:
-				self.word += grapheme
-		self.saveWord()
+		self.saveDict()
 
 	def saveWord(self):
+		'''
+			Saves a complete stored word into the phrase, then resets the stored word.
+			The results are saved in self.word
+			---
+			When a word is being formed:
+			> its stored in a string in self.word
+			When a word is complete:
+			> its put into the self.phrase array, then reset.
+		'''
 		if len(self.phrase)  < self.order:
 			self.phrase += [self.word]
 		if len(self.phrase) == self.order:
@@ -26,6 +26,15 @@ class Markov(object):
 		self.word = ''
 
 	def savePhrase(self):
+		'''
+			Saves a complete stored phrase into the dictionary, then resets the stored phrase.
+			The results are saved in self.phrase
+			---
+			When a phrase is being formed:
+			> its stored in an array in self.phrase
+			When a phrase is complete:
+			> its put into the self.dict dictionary, then reset.
+		'''
 		phrase = tuple(self.phrase)
 		if phrase in self.dict:
 			self.dict[phrase] += 1
@@ -33,6 +42,26 @@ class Markov(object):
 			self.dict[phrase]  = 1
 		del self.phrase[0]
 
+	def saveDict(self):
+		'''
+			Creates a markov chain by looping through each letter in the corpus.
+			The results are saved in self.dict
+			---
+			When a dictionary is being formed:
+			> its stored in a histogram in self.dict
+			When a dictionary is complete:
+			> its put into the self.dict dictionary.
+		'''
+		self.dict = {}
+		for grapheme in self.corpus:
+			if grapheme == ' ': # this can be improved greatly
+				self.saveWord()
+			else:
+				self.word += grapheme
+		self.saveWord()
+
+
 if __name__ == '__main__':
 	fishy = "one fish two fish red fish blue fish"
-	model = Markov(fishy, 5)
+	model = Markov(fishy, 2)
+	print(model.dict)
