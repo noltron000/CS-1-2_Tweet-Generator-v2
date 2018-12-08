1c10ebb67d75ba415df7fde37e3fb212bb6bb97f
Nolan Kovacik <noltron000@gmail.com>
2018-12-08 14:05:14 -0800

    updating listogram

--- misc/dictogram.py
+++ misc/dictogram.py
@@ -3,10 +3,14 @@
 from __future__ import division, print_function  # Python 2 and 3 compatibility
 
 class Dictogram(dict):
-	"""Dictogram is a histogram implemented as a subclass of the dict type."""
+	"""
+		Dictogram is a histogram implemented as a subclass of the dict type.
+	"""
 
 	def __init__(self, word_list=None):
-		"""Initialize this histogram as a new dict and count given words."""
+		"""
+			Initialize this histogram as a new dict and count given words.
+		"""
 		super(Dictogram, self).__init__()  # Initialize this as a new dict
 		# Add properties to track useful word counts for this histogram
 		self.types = 0  # Count of distinct word types in this histogram
@@ -17,8 +21,10 @@ class Dictogram(dict):
 				self.add_count(word)
 
 	def add_count(self, word, count=1):
-		"""Increase frequency count of given word by given count amount."""
-		# TODO: Increase word frequency by count
+		"""
+			Increase frequency count of given word by given count amount.
+		"""
+		# Increase word frequency by count
 		self.tokens += count
 		if word in self:
 			self[word] += count
@@ -28,8 +34,10 @@ class Dictogram(dict):
 			self[word] = count
 
 	def frequency(self, word):
-		"""Return frequency count of given word, or 0 if word is not found."""
-		# TODO: Retrieve word frequency count
+		"""
+			Return frequency count of given word, or 0 if word is not found.
+		"""
+		# Retrieve word frequency count
 		if word in self:
 			return self[word]
 		else:
--- misc/listogram.py
+++ misc/listogram.py
@@ -4,10 +4,14 @@ from __future__ import division, print_function  # Python 2 and 3 compatibility
 
 
 class Listogram(list):
-	"""Listogram is a histogram implemented as a subclass of the list type."""
+	"""
+		Listogram is a histogram implemented as a subclass of the list type.
+	"""
 
 	def __init__(self, word_list=None):
-		"""Initialize this histogram as a new list and count given words."""
+		"""
+			Initialize this histogram as a new list and count given words.
+		"""
 		super(Listogram, self).__init__()  # Initialize this as a new list
 		# Add properties to track useful word counts for this histogram
 		self.types = 0  # Count of distinct word types in this histogram
@@ -15,12 +19,15 @@ class Listogram(list):
 		# Count words in given list, if any
 		print("SELF:")
 		print(self)
+		print(word_list)
 		if word_list is not None:
 			for word in word_list:
 				self.add_count(word)
 
 	def add_count(self, word, count=1):
-		"""Increase frequency count of given word by given count amount."""
+		"""
+			Increase frequency count of given word by given count amount.
+		"""
 		# TODO: Increase word frequency by count
 		self.tokens += count
 		for array in self:
@@ -32,17 +39,42 @@ class Listogram(list):
 
 
 	def frequency(self, word):
-		"""Return frequency count of given word, or 0 if word is not found."""
-		# TODO: Retrieve word frequency count
+		"""
+			Return frequency count of given word, or 0 if word is not found.
+		"""
+		# Retrieve word frequency count
+		for pairing in self:
+			if pairing[0] == word:
+				return pairing[1]
+		else:
+			return 0
 
 	def __contains__(self, word):
-		"""Return boolean indicating if given word is in this histogram."""
-		# TODO: Check if word is in this histogram
+		"""
+			Return boolean indicating if given word is in this histogram.
+		"""
+		# Check if word is in this histogram
+		for pairing in self:
+			if pairing[0] == word:
+				return True
+		else:
+			return False
 
-	def _index(self, target):
-		"""Return the index of entry containing given target word if found in
-		this histogram, or None if target word is not found."""
-		# TODO: Implement linear search to find index of entry with target word
+	def _index(self, word):
+		"""
+			Return the index of entry containing given word if found
+			in this histogram, or None if given word is not found.
+		"""
+		# Implement linear search to find index of entry with given word
+		index = 0
+		for pairing in self:
+			if pairing[0] == word:
+				return index
+			else:
+				index += 1
+		else:
+			return None
+
 
 
 def print_histogram(word_list):
