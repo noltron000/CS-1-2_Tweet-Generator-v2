26ebe642ccb4258a791582f7ed7c62766a3632ac
Nolan Kovacik <noltron000@gmail.com>
2018-12-19 04:07:54 -0500

    adding tuplegram

---
+++ misc/tuplegram.py
@@ -0,0 +1,116 @@
+#!python
+
+from __future__ import division, print_function  # Python 2 and 3 compatibility
+
+
+class Tuplegram(list):
+	"""
+		Tuplegram is a histogram implemented as a subclass of the list type.
+	"""
+
+	def __init__(self, word_list=None):
+		"""
+			Initialize this histogram as a new list and count given words.
+		"""
+		super(Tuplegram, self).__init__()  # Initialize this as a new list
+		# Add properties to track useful word counts for this histogram
+		self.types = 0  # Count of distinct word types in this histogram
+		self.tokens = 0  # Total count of all word tokens in this histogram
+		# Count words in given list, if any
+		print("SELF:")
+		print(self)
+		print(word_list)
+		if word_list is not None:
+			for word in word_list:
+				self.add_count(word)
+
+	def add_count(self, word, count=1):
+		"""
+			Increase frequency count of given word by given count amount.
+		"""
+		# TODO: Increase word frequency by count
+		self.tokens += count
+		index = 0
+		for item in self:
+			index += 1
+			if item[0] == word:
+				newItem = tuple([item[0], item[1]+count])
+				self.remove(item)
+				self.append(newItem)
+				break
+		else:
+			self.types += 1
+			self.append(tuple([word,count]))
+
+
+	def frequency(self, word):
+		"""
+			Return frequency count of given word, or 0 if word is not found.
+		"""
+		# Retrieve word frequency count
+		for pairing in self:
+			if pairing[0] == word:
+				return pairing[1]
+		else:
+			return 0
+
+	def __contains__(self, word):
+		"""
+			Return boolean indicating if given word is in this histogram.
+		"""
+		# Check if word is in this histogram
+		for pairing in self:
+			if pairing[0] == word:
+				return True
+		else:
+			return False
+
+	def _index(self, word):
+		"""
+			Return the index of entry containing given word if found
+			in this histogram, or None if given word is not found.
+		"""
+		# Implement linear search to find index of entry with given word
+		index = 0
+		for pairing in self:
+			if pairing[0] == word:
+				return index
+			else:
+				index += 1
+		else:
+			return None
+
+
+
+def print_histogram(word_list):
+	print('word list: {}'.format(word_list))
+	# Create a tuplegram and display its contents
+	histogram = Tuplegram(word_list)
+	print('tuplegram: {}'.format(histogram))
+	print('{} tokens, {} types'.format(histogram.tokens, histogram.types))
+	for array in histogram:
+		word = array[0]
+		freq = array[1]
+		print('{!r} occurs {} times'.format(word, freq))
+	print()
+
+
+def main():
+	import sys
+	arguments = sys.argv[1:]  # Exclude script name in first argument
+	if len(arguments) >= 1:
+		# Test histogram on given arguments
+		print_histogram(arguments)
+	else:
+		# Test histogram on letters in a word
+		word = 'abracadabra'
+		print_histogram(list(word))
+		# Test histogram on words in a classic book title
+		fish_text = 'one fish two fish red fish blue fish'
+		print_histogram(fish_text.split())
+		# Test histogram on words in a long repetitive sentence
+		woodchuck_text = ('how much wood would a wood chuck chuck\nif a wood chuck could chuck wood')
+		print_histogram(woodchuck_text.split())
+
+if __name__ == '__main__':
+	main()
